879e64f8e53fa3583b9327f0ba0fa77f1bbafb6e
Zhiyad Harith <zeeharith@gmail.com>
2023-04-05 15:51:38 +0800

    first commit

---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot} from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+
+const Customizer = () => {
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
+          >
+            
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Customizer
